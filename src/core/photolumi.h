dac31ca82f292b92aee5caf9aaeb1a173c97477f
Haomiao Jiang <haomiao.jiang@oculus.com>
2019-10-27 20:14:05 -0700

    Update the interpolation logic for PhotoLumi matrix
    We update the interpolation method to make sure that we won't introduce
    fluorescent component by the interpolation. That is, if the input is diagonal
    matrix, the output should still be diagonal.

--- src/core/photolumi.h
+++ src/core/photolumi.h
@@ -10,7 +10,6 @@
 #ifndef PBRT_CORE_PHOTOLUMI_H
 #define PBRT_CORE_PHOTOLUMI_H
 
-// core/photolumi.h*
 #include "pbrt.h"
 #include "stringprint.h"
 #include <Eigen/Dense>
@@ -20,8 +19,6 @@
 namespace pbrt {
 
 enum class PhotoLumiType { Reflectance, Illuminant, Display };
-extern void Blackbody(const Float *lambda, int n, Float T, Float *Le);
-extern void BlackBodyNormalized(const Float *lambda, int n, Float T, Float *vals);
 
 class PhotoLumi : public Eigen::Matrix<
     Float, nSpectralSamples, nSpectralSamples> {
@@ -44,40 +41,71 @@ class PhotoLumi : public Eigen::Matrix<
 
   static PhotoLumi FromSampled(const std::vector<Float>& lambda,
       const std::vector<std::vector<Float>> &v) {
-    DCHECK(lambda.size() == v.size());
+    DCHECK(lambda.size() == v.size() && lambda.size() > 1);
 
-    // Perform average interpolation along each direction independently.
-    // TODO: This averaging logic is incorrect because the numbers in the
-    // lambda matrix is not the spectrum range energy. Instead, it only
-    // represents a ratio between in and out light. Thus, the wavelength
-    // interpolation should be similar to the a normal interpolation, with
-    // fluorescent and normal reflection (i.e. the diagonal) interpolated
-    // independently.
+    // The values in the input matrix represents a ratio between in and out
+    // light. Thus, the wavelength interpolation should be similar to the a
+    // normal interpolation and is different from the one in Spectrum class.
+    // However, special care is needed and we cannot use vanilla 2d
+    // interpolation:
+    //   We would like non-fluorescent (diagonal) matrix to remain diagonal
+    //   after interpolation. That is, we don't want to introduce non-existence
+    //   fluorescent in the interpolation.
+    // Thus, we perform fluorescent and normal reflection (i.e. the diagonal)
+    // interpolated independently.
+
+    // Store non-fluorescent part to be interpolated later.
+    PhotoLumi result;
+    Eigen::Array<Float, 1, Eigen::Dynamic> nonFluorescent(lambda.size());
+    for (int i = 0; i < lambda.size(); i++) {
+      nonFluorescent(i) = v[i][i];
+    }
+
+    // Compute scaled / normalized input and output wavelength.
+    Eigen::Array<Float, 1, Eigen::Dynamic> scaled_lambda = (Eigen::Map<
+        const Eigen::Array<Float, 1, Eigen::Dynamic>>(
+            lambda.data(), lambda.size()) - lambda[0]) / (
+                lambda.back() - lambda[0]);
+    Eigen::Array<Float, 1, nSpectralSamples> scaled_output_lambda = (
+        Eigen::Array<Float, 1, nSpectralSamples>::LinSpaced(
+            sampledLambdaStart, sampledLambdaEnd, nSpectralSamples) -
+            lambda[0]) / (lambda.back() - lambda[0]);
+    scaled_output_lambda = scaled_output_lambda.max(0.0f).min(1.0f);
+
+    // Interpolate for fluorescent part.
     Eigen::Array<Float, Eigen::Dynamic, nSpectralSamples> intermediate_result(
         lambda.size(), nSpectralSamples);
     for (int i = 0; i < lambda.size(); ++i) {
-      for (int j = 0; j < nSpectralSamples; ++j) {
-        Float lambda0 = Lerp(Float(j) / Float(nSpectralSamples),
-                             sampledLambdaStart, sampledLambdaEnd);
-        Float lambda1 = Lerp(Float(j + 1) / Float(nSpectralSamples),
-                             sampledLambdaStart, sampledLambdaEnd);
-        intermediate_result(i, j) = AverageSpectrumSamples(
-            lambda.data(), v[i].data(), lambda.size(), lambda0, lambda1);
+      Eigen::Array<Float, 1, Eigen::Dynamic> current_row = Eigen::Map<
+          const Eigen::Array<Float, 1, Eigen::Dynamic>>(
+              v[i].data(), lambda.size());
+      // remove non-fluorescent part.
+      // Maybe it's better to actual remove this data point in interpolation.
+      // But assuming that fluorescent happens at far enough wavelength from
+      // input light, it should be safe to just set it to zero.
+      current_row[i] = 0.0f;
+      Eigen::Spline<Float, 1> spline(Eigen::SplineFitting<
+          Eigen::Spline<Float, 1>>::Interpolate(current_row, 2, scaled_lambda));
+      for (int j = 0; j < nSpectralSamples; j++) {
+        intermediate_result(i, j) = spline(scaled_output_lambda(j))(0);
       }
     }
 
-    PhotoLumi result;
     for (int j = 0; j < nSpectralSamples; j++) {
-      Eigen::Matrix<Float, nSpectralSamples, 1> v_column =
-          intermediate_result.col(j);
+      Eigen::Spline<Float, 1> spline(Eigen::SplineFitting<
+          Eigen::Spline<Float, 1>>::Interpolate(
+              intermediate_result.col(j).transpose(), 2, scaled_lambda));
       for (int i = 0; i < nSpectralSamples; i++) {
-        Float lambda0 = Lerp(Float(i) / Float(nSpectralSamples),
-                             sampledLambdaStart, sampledLambdaEnd);
-        Float lambda1 = Lerp(Float(i + 1) / Float(nSpectralSamples),
-                             sampledLambdaStart, sampledLambdaEnd);
-        result(i, j) = AverageSpectrumSamples(
-            lambda.data(), v_column.data(), lambda.size(), lambda0, lambda1);
+        result(i, j) = spline(scaled_output_lambda(i))(0);
       }
+    }
+
+    // Interpolate for non-fluorescent part.
+    Eigen::Spline<Float, 1> spline(Eigen::SplineFitting<
+        Eigen::Spline<Float, 1>>::Interpolate(
+            nonFluorescent, 2, scaled_lambda));
+    for (int i = 0; i < nSpectralSamples; i++) {
+      result(i, i) = spline(scaled_output_lambda(i))(0);
     }
     return result;
   }
